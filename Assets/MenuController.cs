e4d098e7eb0c3482480cea83a2e73daf2ccf3b3d
BarsikTheCaT <barsikdacat@gmail.com>
2018-03-31 19:25:53 +1000

    =\

---
+++ Assets/MenuController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MenuController : MonoBehaviour
+{
+	public Canvas Overlay;
+	public Canvas Menu;
+	/// <summary>
+	/// Game's main menu.
+	/// </summary>
+	private Scene MenuScene;
+	/// <summary>
+	/// Game's main scene.
+	/// </summary>
+	private Scene GameScene;
+	/// <summary>
+	/// Starts a new game.
+	/// </summary>
+	public void StartGame()
+	{
+		SceneManager.LoadSceneAsync(1);
+		SceneManager.SetActiveScene(GameScene);
+	}
+	/// <summary>
+	/// Continues current game.
+	/// </summary>
+	public void ContinueGame()
+	{
+		Time.timeScale = 1;
+		Menu.gameObject.SetActive(false);
+		Overlay.gameObject.SetActive(true);
+	}
+	/// <summary>
+	/// Returns to main menu.
+	/// </summary>
+	public void Leave()
+	{
+		SceneManager.SetActiveScene(MenuScene);
+		SceneManager.UnloadSceneAsync(GameScene);
+	}
+	/// <summary>
+	/// Pauses the game.
+	/// </summary>
+	public void PauseGame()
+	{
+		Time.timeScale = 0;
+		Overlay.gameObject.SetActive(false);
+		Menu.gameObject.SetActive(true);
+	}
+	// Use this for initialization
+	void Start () {
+		MenuScene = SceneManager.GetSceneByBuildIndex(1);
+		GameScene = SceneManager.GetSceneByBuildIndex(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/NavigationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationController : MonoBehaviour {
+	public GameObject Destination;
+	private NavMeshAgent navMeshAgent;
+	private double TimeAtDestination = 0f;
+	// Use this for initialization
+	void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		navMeshAgent.destination = Destination.transform.localPosition;
+		navMeshAgent.isStopped = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (((transform.position - navMeshAgent.destination).magnitude) <= 4) { TimeAtDestination += Time.deltaTime; };
+		if (TimeAtDestination > 1) { navMeshAgent.isStopped = true; }
+	}
+}
